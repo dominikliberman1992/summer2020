245f14f3e2b85b2329c2f4f2ee4ca1ca24bc1212
dominikliberman1992 <44137006+dominikliberman1992@users.noreply.github.com>
2020-03-03 10:02:36 +0200

    Update exercise5-fix_server1.sh

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
-ssh-keygen
-sudo passwd vagrant
-ssh-copy-id server2
+ssh-keygen -b 2048 -t rsa -N "" -f /home/vagrant/.ssh/id_rsa
+sudo cp /home/vagrant/.ssh/id_rsa.pub /vagrant/
