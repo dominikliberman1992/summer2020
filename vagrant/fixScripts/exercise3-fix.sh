32b4d066bfddf29ca9c8f59d11d93cda00a0345b
dominikliberman1992 <44137006+dominikliberman1992@users.noreply.github.com>
2020-03-03 09:32:40 +0200

    Update exercise3-fix.sh

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 #add fix to exercise3 here
-sudo vi /etc/apache2/sites-available/default
-        <Directory /var/www/>
-                Options Indexes FollowSymLinks MultiViews
-                AllowOverride None
-                Order allow,deny
-                allow from all
-        </Directory>
+sudo sed  -i  's/deny from all/Allow from all/i'  /etc/apache2/sites-available/default
+service  apache2 reload
