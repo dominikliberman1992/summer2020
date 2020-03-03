5f79e2d84a4ec1bf2eb2c2c060df9ea4f86526ef
dominikliberman1992 <44137006+dominikliberman1992@users.noreply.github.com>
2020-03-03 09:12:48 +0200

    Update exercise1-fix.sh

--- vagrant/fixScripts/exercise1-fix.sh
+++ vagrant/fixScripts/exercise1-fix.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 #add fix to exercise1 here
-add dns the server 
-sudo vi /etc/network/interfaces
-auto eth1
-iface eth1 inet static
-      address 192.168.100.10
-      netmask 255.255.255.0
-      dns-nameserver 8.8.8.8 8.8.8.4
+sudo sh -c  'echo nameserver 8.8.8.8 >> /etc/resolv.conf' 
