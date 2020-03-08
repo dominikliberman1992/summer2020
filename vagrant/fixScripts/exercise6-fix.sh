724cc74a2e2e13d9b9953b564904153076dee792
dominikliberman1992 <44137006+dominikliberman1992@users.noreply.github.com>
2020-03-08 14:36:20 +0200

    Update exercise6-fix.sh

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 #add fix to exercise6-fix here
-read -p "Enter the server or IP you want to copy to " server
-read -p "Enter path '/SRC/ or /DEST/namefile.txt'  you wants to copy " copy1
-read -p "Enter path '/SRC/ or /DEST/namefile.txt' you wants to seva the copy " copy2
-rsync  -vrlHh --stats vagrant@$server:$copy1 $copy2 | grep "Number of files transferred:" | cut -d " " -f 5
+rsync  -vrlHh --stats vagrant@server2:/tmp/ /tmp/ | grep "Number of files transferred:" | cut -d " " -f 5
